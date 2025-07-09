3922af65d3813dd5b3828407aea6aa3f21fec846
LucaBertoli <lucabertoli10@yahoo.it>
2025-07-09 15:14:24 +0000

    commit readme

---
+++ README.md
@@ -0,0 +1,1 @@
+Questa repository contiene la collezione di script per il calcolo dell'identità su read sequenziate.
--- Run_Identity_Workflow.sh
+++ Run_Identity_Workflow.sh
@@ -21,29 +21,31 @@ FASTA="/home/db/hg38_bwa2/hg38_chr1-22-XYM/Homo_sapiens_assembly38_noalt.fasta"
 BED="/home/db/hg38_bwa2/hg38_chr1-22-XYM/Homo_sapiens_assembly38_noalt.bed"
 
 
-# bedtools intersect -abam $bam_temporaneo -b /home/comparazione_kit/calcolo_identità_sequenze_Sequenziatori/HG001_GRCh38_1_22_v4.2.1_benchmark.bed -f 1 -u > $bam
-# samtools index $bam
+bedtools intersect -abam $bam_temporaneo -b /home/comparazione_kit/calcolo_identità_sequenze_Sequenziatori/HG001_GRCh38_1_22_v4.2.1_benchmark.bed -f 1 -u > $bam
+samtools index $bam
 
-# echo "Step1: INIZIO calcolo dell'identità sul bam..."
-# python $scripts_folder/IdentityRevelations.py $bam $vcf IDENTITY_${output_name}.tsv.gz
-# echo "Step1: FINE calcolo dell'identità sul bam..."
+echo "Step1: INIZIO calcolo dell'identità sul bam..."
+python $scripts_folder/IdentityRevelations.py $bam $vcf IDENTITY_${output_name}.tsv.gz
+echo "Step1: FINE calcolo dell'identità sul bam..."
 
-# echo "Step2: INIZIO calcolo delle statistiche sull'identità del bam..."
-# python $scripts_folder/IdentityRevelations_stats.py IDENTITY_${output_name}.tsv.gz >> IDENTITY_${output_name}.tsv.gz.stats
-# echo "Step2: FINE calcolo delle statistiche sull'identità del bam..."
+echo "Step2: INIZIO calcolo delle statistiche sull'identità del bam..."
+python $scripts_folder/IdentityRevelations_stats.py IDENTITY_${output_name}.tsv.gz >> IDENTITY_${output_name}.tsv.gz.stats
+echo "Step2: FINE calcolo delle statistiche sull'identità del bam..."
 
-# echo "Step3: INIZIO calcolo delle qualità medie delle read sequenziate..."
-# python $scripts_folder/Scatter_BAM.py $bam QUAL
-# echo "Step3: FINE calcolo delle qualità medie delle read sequenziate..."
+echo "Step3: INIZIO calcolo delle qualità medie delle read sequenziate..."
+python $scripts_folder/Scatter_BAM.py $bam QUAL
+echo "Step3: FINE calcolo delle qualità medie delle read sequenziate..."
 
-# echo "Step4: INIZIO plot delle qualità stimate dal sequenziatore e calcolate in base all'identità..."
-# python $scripts_folder/Identity_plot.py IDENTITY_${output_name}.tsv.gz $(dirname $bam)/$(basename $bam)_mean_quality.tsv.gz IDENTITY_$output_name
-# echo "Step4: FINE plot delle qualità stimate dal sequenziatore e calcolate in base all'identità..."
+echo "Step4: INIZIO plot delle qualità stimate dal sequenziatore e calcolate in base all'identità..."
+python $scripts_folder/Identity_plot.py IDENTITY_${output_name}.tsv.gz $(dirname $bam)/$(basename $bam)_mean_quality.tsv.gz IDENTITY_$output_name
+echo "Step4: FINE plot delle qualità stimate dal sequenziatore e calcolate in base all'identità..."
 
 echo "Step5: INIZIO calcolo dell'identità utilizzando tutti i dati assieme..."
 bash $scripts_folder/identity_calculalculation_all_types.sh $FASTA $bam $vcf $BED IDENTITY_${output_name}
 echo "Step5: FINE calcolo dell'identità utilizzando tutti i dati assieme..."
 
+
+#### I seguenti comandi non vanno utilizzati a meno che non si voglia calcolare l'identità separando read 1 e read 2.
 # echo "Step1.1: INIZIO calcolo dell'identità sul bam read1..."
 # python $scripts_folder/IdentityRevelations.py $bam $vcf IDENTITY_${output_name}_read1.tsv.gz read1
 # echo "Step1.1: FINE calcolo dell'identità sul bam read1..."
