b3f6dca0c5494977a08f8c86abf267a32495ab19
LucaBertoli <lucabertoli10@yahoo.it>
2025-07-16 09:27:47 +0000

    crea bam con read >=Q30

--- Scatter_BAM.py
+++ Scatter_BAM.py
@@ -11,9 +11,11 @@ def process_bam(input_bam, mode):
     write_bams = mode in ("BOTH", "BAM")
     write_tsv = mode in ("BOTH", "QUAL")
 
+    #condizione per aprire in modalità scrittura dei file bam (da utilzizare se si vogliono generare bam con soltanto read di una certa qualità)
     if write_bams:
-        bam_out_30_35 = pysam.AlignmentFile(input_bam + "_reads_30_35.bam", "wb", template=bam_in)
-        bam_out_35_up = pysam.AlignmentFile(input_bam + "_reads_35_up.bam", "wb", template=bam_in)
+        bam_out_30_up = pysam.AlignmentFile(input_bam + "_reads_30_up.bam", "wb", template=bam_in)
+        # bam_out_30_35 = pysam.AlignmentFile(input_bam + "_reads_30_35.bam", "wb", template=bam_in)
+        # bam_out_35_up = pysam.AlignmentFile(input_bam + "_reads_35_up.bam", "wb", template=bam_in)
 
     if write_tsv:
         out_tsv = gzip.open(input_bam + "_mean_quality.tsv.gz", "wt")
@@ -29,11 +31,14 @@ def process_bam(input_bam, mode):
         if write_tsv:
             out_tsv.write(f"{read.query_name}\t{mean_qual:.2f}\n")
 
+        #condizione per scrivere uno o piu bam con read di una certa qualità
         if write_bams:
-            if 30 <= mean_qual < 35:
-                bam_out_30_35.write(read)
-            elif mean_qual >= 35:
-                bam_out_35_up.write(read)
+            if 30 <= mean_qual:
+                bam_out_30_up.write(read)
+            # if 30 <= mean_qual < 35:
+            #     bam_out_30_35.write(read)
+            # elif mean_qual >= 35:
+            #     bam_out_35_up.write(read)
 
     bam_in.close()
 
@@ -58,71 +63,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-# import pysam
-# import sys
-# import statistics
-# import gzip
-
-# def write_mean_quality_tsv_gz(input_bam, output_tsv_gz):
-#     bam_in = pysam.AlignmentFile(input_bam, "rb")
-#     with gzip.open(output_tsv_gz, "wt") as out_bam_qual:
-#         out_bam_qual.write("read_name\tmean_quality\n")
-#         for read in bam_in.fetch(until_eof=True):
-#             if read.query_alignment_qualities:
-#                 mean_qual = statistics.mean(read.query_alignment_qualities)
-#                 out_bam_qual.write(f'{read.query_name}\t{mean_qual:.2f}\n')
-#     bam_in.close()
-
-# def write_bam_by_quality(input_bam, out_bam_30_35, out_bam_35_up):
-#     bam_in = pysam.AlignmentFile(input_bam, "rb")
-#     bam_out_30_35 = pysam.AlignmentFile(out_bam_30_35, "wb", template=bam_in)
-#     bam_out_35_up = pysam.AlignmentFile(out_bam_35_up, "wb", template=bam_in)
-#     for read in bam_in.fetch(until_eof=True):
-#         if read.query_alignment_qualities:
-#             mean_qual = statistics.mean(read.query_alignment_qualities)
-#             if 30 <= mean_qual < 35:
-#                 bam_out_30_35.write(read)
-#             elif mean_qual >= 35:
-#                 bam_out_35_up.write(read)
-#     bam_in.close()
-#     bam_out_30_35.close()
-#     bam_out_35_up.close()
-
-# def main():
-#     if len(sys.argv) != 3:
-#         print(f"Uso: python ScatterBam.py input.bam BAM|QUAL|BOTH")
-#         sys.exit(1)
-
-#     input_bam = sys.argv[1]
-#     mode = sys.argv[2].upper()
-
-#     print(f"Input BAM: {input_bam}")
-#     print(f"Mode: {mode}")
-    
-#     if mode in ("BOTH", "BAM"):
-#         print(f"INIZIO separazione delle read in base alla qualità media...")
-#         write_bam_by_quality(
-#             input_bam,
-#             input_bam + "_reads_30_35.bam",
-#             input_bam + "_reads_35_up.bam"
-#         )
-#         print(f"FINE primo step...")
-
-    
-#     if mode in ("BOTH", "QUAL"):
-#         print(f"INIZIO calcolo delle qualità medie delle read...")
-#         write_mean_quality_tsv_gz(
-#             input_bam, 
-#             input_bam + "_mean_quality.tsv.gz"
-#         )
-#         print(f"FINE secondo step...")
-# ò
-# if __name__ == "__main__":
-#     main()
-
