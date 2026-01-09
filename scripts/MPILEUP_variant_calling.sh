629397d87f4c556c09a7d4542fe7a1090f5701b9
LucaBertoli <lucabertoli10@yahoo.it>
2026-01-09 11:06:44 +0000

    adding script for variant calling with bcftools mpileup and update of plot script

--- scripts/MPILEUP_variant_calling.sh
+++ scripts/MPILEUP_variant_calling.sh
@@ -1,5 +1,22 @@
 bam=$1
 
+# bcftools mpileup \
+#   -f /home/db/hg38_bwa2/hg38_chr1-22-XYM/Homo_sapiens_assembly38_noalt.fasta \
+#   -a AD,DP \
+#   -Q 0 \
+#   -q 0 \
+#   -B \
+#   --threads 10 \
+#   $bam \
+# | bcftools call \
+#   -A \
+#   -m \
+#   -v \
+#   --ploidy 1 \
+#   --threads 10 \
+#   -Oz \
+#   -o $(basename $bam .bam)_mpileup_permissive.vcf.gz
+
 bcftools mpileup \
   -f /home/db/hg38_bwa2/hg38_chr1-22-XYM/Homo_sapiens_assembly38_noalt.fasta \
   -a AD,DP \
@@ -8,14 +25,13 @@ bcftools mpileup \
   -B \
   --threads 10 \
   $bam \
-| bcftools call \
-  -m \
-  --ploidy 1 \
-  --threads 10 \
+| bcftools view \
   -Oz \
-  -o $(basename $bam .bam)_mpileup_permissive.vcf.gz
+  -i 'TYPE!="ref"' \
+  -o $(basename $bam .bam)_mpileup_raw.vcf.gz
 
-tabix $(basename $bam .bam)_mpileup_permissive.vcf.gz
 
-/home/tools/bcftools-1.19/bcftools norm $(basename $bam .bam)_mpileup_permissive.vcf.gz -f /home/db/hg38_bwa2/hg38_chr1-22-XYM/Homo_sapiens_assembly38_noalt.fasta -m - both | bgzip > $(basename $bam .bam)_mpileup_permissive_split.vcf.gz
-tabix $(basename $bam .bam)_mpileup_permissive_split.vcf.gz
+# tabix $(basename $bam .bam)_mpileup_permissive.vcf.gz
+
+# /home/tools/bcftools-1.19/bcftools norm $(basename $bam .bam)_mpileup_permissive.vcf.gz -f /home/db/hg38_bwa2/hg38_chr1-22-XYM/Homo_sapiens_assembly38_noalt.fasta -m - both | bgzip > $(basename $bam .bam)_mpileup_permissive_split.vcf.gz
+# tabix $(basename $bam .bam)_mpileup_permissive_split.vcf.gz
